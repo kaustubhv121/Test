da45cd835128ca3fa3d448d2ea41cdde02a3e0fa
kaustubhv121 <kaustubhv121@gmail.com>
2015-07-02 09:29:04 -0500

    Update obd_recorder.py

--- obd_recorder.py
+++ obd_recorder.py
@@ -18,18 +18,14 @@ class OBD_Recorder():
         localtime = time.localtime(time.time())
         filename = path+"car-"+str(localtime[0])+"-"+str(localtime[1])+"-"+str(localtime[2])+"-"+str(localtime[3])+"-"+str(localtime[4])+"-"+str(localtime[5])+".log"
         self.log_file = open(filename, "w", 128)
-        self.log_file.write("Time,RPM,MPH,MAF,ERROR CODES\n");
+        self.log_file.write("ERROR CODES\n");
 
         for item in log_items:
             self.add_log_item(item)
 
-        self.gear_ratios = [34/13, 39/21, 36/23, 27/20, 26/21, 25/22]
-        #log_formatter = logging.Formatter('%(asctime)s.%(msecs).03d,%(message)s', "%H:%M:%S")
-
     def connect(self):
         portnames = scanSerial()
-        #portnames = ['COM10']
-        print portnames
+
         for port in portnames:
             self.port = obd_io.OBDPort(port, None, 2, 2)
             if(self.port.State == 0):
@@ -37,9 +33,6 @@ class OBD_Recorder():
                 self.port = None
             else:
                 break
-
-        if(self.port):
-            print "Connected to "+self.port.port.name
             
     def is_connected(self):
         return self.port
@@ -48,53 +41,21 @@ class OBD_Recorder():
         for index, e in enumerate(obd_sensors.SENSORS):
             if(item == e.shortname):
                 self.sensorlist.append(index)
-                print "Logging item: "+e.name
                 break
             
             
     def record_data(self):
         if(self.port is None):
             return None
-        
-        print "Logging started"
-        
+       
         while 1:
-            localtime = datetime.now()
-            #current_time = str(localtime.hour)+":"+str(localtime.minute)+":"+str(localtime.second)+"."+str(localtime.microsecond)
-            log_string = ""
-            results = {}
             for index in self.sensorlist:
                 (name, value, unit) = self.port.sensor(index)
-                log_string = log_string + ","+str(value)
-                results[obd_sensors.SENSORS[index].shortname] = value;
-
-            gear = self.calculate_gear(results["rpm"], results["speed"])
-            log_string = log_string #+ "," + str(gear)
-            self.log_file.write(log_string+"\n")
-
+            self.log_file.write(str(value)+"\n")
+            print str(value)+"\n"
             
-    def calculate_gear(self, rpm, speed):
-        if speed == "" or speed == 0:
-            return 0
-        if rpm == "" or rpm == 0:
-            return 0
-
-        rps = rpm/60
-        mps = (speed*1.609*1000)/3600
-        
-        primary_gear = 85/46 #street triple
-        final_drive  = 47/16
-        
-        tyre_circumference = 1.978 #meters
-
-        current_gear_ratio = (rps*tyre_circumference)/(mps*primary_gear*final_drive)
-        
-        #print current_gear_ratio
-        gear = min((abs(current_gear_ratio - i), i) for i in self.gear_ratios)[1] 
-        return gear
-        
 username = getpass.getuser()  
-logitems = ["rpm", "speed", "maf", "dtc_status"]
+logitems = ["dtc_status"]
 o = OBD_Recorder('/home/'+username+'/Test/log/', logitems)
 o.connect()
 
